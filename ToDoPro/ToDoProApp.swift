85f9a188853e92280fa1e301af7bffd944b502ea
Youran Tao Jensen <mps348@alumni.ku.dk>
2025-10-01 12:09:37 +0200

    Initial Commit

---
+++ ToDoPro/ToDoProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoProApp.swift
+//  ToDoPro
+//
+//  Created by Youran Tao Jensen on 01/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoProTests/ToDoProTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoProTests.swift
+//  ToDoProTests
+//
+//  Created by Youran Tao Jensen on 01/10/2025.
+//
+
+import Testing
+
+struct ToDoProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
